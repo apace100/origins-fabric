8f6b15475999b2f57841608c4db6a76a4c6073cf
Apace <apace100@web.de>
2020-07-07 13:24:18 +0200

    Added a gui to view your current origin, by default by pressing O

--- src/main/java/io/github/apace100/origins/OriginsClient.java
+++ src/main/java/io/github/apace100/origins/OriginsClient.java
@@ -4,6 +4,7 @@ import io.github.apace100.origins.networking.ModPackets;
 import io.github.apace100.origins.networking.ModPacketsS2C;
 import io.github.apace100.origins.registry.ModBlocks;
 import io.github.apace100.origins.screen.PowerHudRenderer;
+import io.github.apace100.origins.screen.ViewOriginScreen;
 import io.netty.buffer.Unpooled;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
@@ -13,6 +14,7 @@ import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.event.client.ClientTickCallback;
 import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.util.InputUtil;
@@ -22,7 +24,8 @@ import org.lwjgl.glfw.GLFW;
 
 public class OriginsClient implements ClientModInitializer {
 
-    public static KeyBinding keyBinding;
+    public static KeyBinding useActivePowerKeybind;
+    public static KeyBinding viewCurrentOriginKeybind;
 
     @Override
     @Environment(EnvType.CLIENT)
@@ -30,12 +33,20 @@ public class OriginsClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.TEMPORARY_COBWEB, RenderLayer.getCutout());
         ModPacketsS2C.register();
 
-        keyBinding = FabricKeyBinding.Builder.create(new Identifier(Origins.MODID, "active_power"),
+        useActivePowerKeybind = FabricKeyBinding.Builder.create(new Identifier(Origins.MODID, "active_power"),
             InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_G, "category." + Origins.MODID).build();
-        KeyBindingHelper.registerKeyBinding(keyBinding);
+        KeyBindingHelper.registerKeyBinding(useActivePowerKeybind);
+        viewCurrentOriginKeybind = FabricKeyBinding.Builder.create(new Identifier(Origins.MODID, "view_origin"),
+            InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_O, "category." + Origins.MODID).build();
+        KeyBindingHelper.registerKeyBinding(viewCurrentOriginKeybind);
         ClientTickCallback.EVENT.register(client -> {
-            while(keyBinding.wasPressed()) {
+            while(useActivePowerKeybind.wasPressed()) {
                 ClientSidePacketRegistry.INSTANCE.sendToServer(ModPackets.USE_ACTIVE_POWER, new PacketByteBuf(Unpooled.buffer()));
+            }
+            while(viewCurrentOriginKeybind.wasPressed()) {
+                if(!(MinecraftClient.getInstance().currentScreen instanceof ViewOriginScreen)) {
+                    MinecraftClient.getInstance().openScreen(new ViewOriginScreen());
+                }
             }
         });
         new PowerHudRenderer().register();
---
+++ src/main/java/io/github/apace100/origins/screen/ViewOriginScreen.java
@@ -0,0 +1,163 @@
+package io.github.apace100.origins.screen;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import io.github.apace100.origins.Origins;
+import io.github.apace100.origins.origin.Impact;
+import io.github.apace100.origins.origin.Origin;
+import io.github.apace100.origins.power.PowerType;
+import io.github.apace100.origins.registry.ModComponents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.StringRenderable;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+
+import java.util.List;
+
+public class ViewOriginScreen extends Screen {
+
+	private static final Identifier WINDOW = new Identifier(Origins.MODID, "textures/gui/choose_origin.png");
+
+	private Origin origin;
+	private static final int windowWidth = 176;
+	private static final int windowHeight = 182;
+	private int scrollPos = 0;
+	private int currentMaxScroll = 0;
+	private int border = 13;
+
+	private int guiTop, guiLeft;
+
+	public ViewOriginScreen() {
+		super(new TranslatableText(Origins.MODID + ".screen.view_origin"));
+		this.origin = ModComponents.ORIGIN.get(MinecraftClient.getInstance().player).getOrigin();
+	}
+
+	@Override
+	public boolean shouldCloseOnEsc() {
+		return false;
+	}
+
+	@Override
+	protected void init() {
+		super.init();
+		guiLeft = (this.width - windowWidth) / 2;
+        guiTop = (this.height - windowHeight) / 2;
+        addButton(new ButtonWidget(guiLeft + windowWidth / 2 - 50, guiTop + windowHeight + 5, 100, 20, new TranslatableText(Origins.MODID + ".gui.close"), b -> {
+			MinecraftClient.getInstance().openScreen(null);
+        }));
+        Origin.HUMAN.getDisplayItem().getOrCreateTag().putString("SkullOwner", MinecraftClient.getInstance().player.getDisplayName().getString());
+	}
+	
+	@Override
+	public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+		this.renderBackground(matrices);
+		this.renderOriginWindow(matrices, mouseX, mouseY);
+		super.render(matrices, mouseX, mouseY, delta);
+	}
+
+	private void renderOriginWindow(MatrixStack matrices, int mouseX, int mouseY) {
+		RenderSystem.enableBlend();
+		renderWindowBackground(matrices, 16, 0);
+		this.renderOriginContent(matrices, mouseX, mouseY);
+		this.client.getTextureManager().bindTexture(WINDOW);
+		this.drawTexture(matrices, guiLeft, guiTop, 0, 0, windowWidth, windowHeight);
+		renderOriginName(matrices);
+		this.client.getTextureManager().bindTexture(WINDOW);
+		this.renderOriginImpact(matrices, mouseX, mouseY);
+		Text title = new TranslatableText(Origins.MODID + ".gui.view_origin.title");
+		this.drawCenteredString(matrices, this.textRenderer, title.getString(), width / 2, guiTop - 15, 0xFFFFFF);
+		RenderSystem.disableBlend();
+	}
+	
+	private void renderOriginImpact(MatrixStack matrices, int mouseX, int mouseY) {
+		Impact impact = origin.getImpact();
+		int impactValue = impact.getImpactValue();
+		int wOffset = impactValue * 8;
+		for(int i = 0; i < 3; i++) {
+			if(i < impactValue) {
+				this.drawTexture(matrices, guiLeft + 128 + i * 10, guiTop + 19, windowWidth + wOffset, 16, 8, 8);
+			} else {
+				this.drawTexture(matrices, guiLeft + 128 + i * 10, guiTop + 19, windowWidth, 16, 8, 8);
+			}
+		}
+		if(mouseX >= guiLeft + 128 && mouseX <= guiLeft + 158
+		&& mouseY >= guiTop + 19 && mouseY <= guiTop + 27) {
+			TranslatableText ttc = (TranslatableText) new TranslatableText(Origins.MODID + ".gui.impact.impact").append(": ").append(impact.getTextComponent());
+			this.renderTooltip(matrices, ttc, mouseX, mouseY);
+		}
+	}
+	
+	private void renderOriginName(MatrixStack matrices) {
+		StringRenderable originName = textRenderer.trimToWidth(origin.getName(), windowWidth - 36);
+		this.drawStringWithShadow(matrices, textRenderer, originName.getString(), guiLeft + 39, guiTop + 19, 0xFFFFFF);
+		ItemStack is = origin.getDisplayItem();
+		this.itemRenderer.renderInGui(is, guiLeft + 15, guiTop + 15);
+	}
+	
+	private void renderWindowBackground(MatrixStack matrices, int offsetYStart, int offsetYEnd) {
+		int endX = guiLeft + windowWidth - border;
+		int endY = guiTop + windowHeight - border;
+		this.client.getTextureManager().bindTexture(WINDOW);
+		for(int x = guiLeft; x < endX; x += 16) {
+			for(int y = guiTop + offsetYStart; y < endY + offsetYEnd; y += 16) {
+				this.drawTexture(matrices, x, y, windowWidth, 0, Math.max(16, endX - x), Math.max(16, endY + offsetYEnd - y));
+			}
+		}
+	}
+	
+	@Override
+	public boolean mouseScrolled(double x, double y, double z) {
+		boolean retValue = super.mouseScrolled(x, y, z);
+		int np = this.scrollPos - (int)z * 4;
+		this.scrollPos = np < 0 ? 0 : Math.min(np, this.currentMaxScroll);
+		return retValue;
+	}
+
+	private void renderOriginContent(MatrixStack matrices, int mouseX, int mouseY) {
+		int x = guiLeft + 18;
+		int y = guiTop + 50;
+		int startY = y;
+		int endY = y - 72 + windowHeight;
+		y -= scrollPos;
+		
+		Text orgDesc = origin.getDescription();
+		List<StringRenderable> descLines = textRenderer.wrapLines(orgDesc, windowWidth - 36);
+		for(StringRenderable line : descLines) {
+			if(y >= startY - 18 && y <= endY + 12) {
+				textRenderer.draw(matrices, line, x + 2, y - 6, 0xCCCCCC);
+			}
+			y += 12;
+		}
+		
+		for(PowerType<?> p : origin.getPowerTypes()) {
+			if(p.isHidden()) {
+				continue;
+			}
+			StringRenderable name = textRenderer.trimToWidth(p.getName().formatted(Formatting.UNDERLINE), windowWidth - 36);
+			Text desc = p.getDescription();
+			List<StringRenderable> drawLines = textRenderer.wrapLines(desc, windowWidth - 36);
+			if(y >= startY - 24 && y <= endY + 12) {
+				textRenderer.draw(matrices, name, x, y, 0xFFFFFF);
+			}
+			for(StringRenderable line : drawLines) {
+				y += 12;
+				if(y >= startY - 24 && y <= endY + 12) {
+					textRenderer.draw(matrices, line, x + 2, y, 0xCCCCCC);
+				}
+			}
+
+			y += 14;
+			
+		}
+		y += scrollPos;
+		currentMaxScroll = y - windowHeight - 15;
+		if(currentMaxScroll < 0) {
+			currentMaxScroll = 0;
+		}
+	}
+}
