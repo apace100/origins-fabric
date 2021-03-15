c3aab8345205b741011f8705ea723ea2ce555740
Apace <apace100@web.de>
2021-03-15 05:03:29 +0100

    Fixed config screen referencing old config class

--- src/main/java/io/github/apace100/origins/integration/ModMenuIntegration.java
+++ src/main/java/io/github/apace100/origins/integration/ModMenuIntegration.java
@@ -1,7 +1,7 @@
 package io.github.apace100.origins.integration;
 
 import io.github.apace100.origins.Origins;
-import io.github.apace100.origins.util.OriginsConfig;
+import io.github.apace100.origins.OriginsClient;
 import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
@@ -17,6 +17,6 @@ public class ModMenuIntegration implements ModMenuApi {
     @Override
     @Environment(EnvType.CLIENT)
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(OriginsConfig.class, parent).get();
+        return parent -> AutoConfig.getConfigScreen(OriginsClient.ClientConfig.class, parent).get();
     }
 }
