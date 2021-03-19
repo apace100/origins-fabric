e4895f4b119411d3323b38841a0446e735e56f9c
Robert Miles <khuxkm@ttm.sh>
2021-03-19 18:14:30 -0400

    Remove unused NoSuchElementException import
    Used in a prior stage of developing the code when I implemented a catch-all to keep the game from crashing when ticking non-player mob movement.

--- src/main/java/io/github/apace100/origins/mixin/LivingEntityMixin.java
+++ src/main/java/io/github/apace100/origins/mixin/LivingEntityMixin.java
@@ -21,7 +21,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import java.util.NoSuchElementException;
 
 import java.util.List;
 import java.util.Optional;
