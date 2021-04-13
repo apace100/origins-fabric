f8dd94a69e5ad0a740d3041eabc569b493531c34
Apace <apace100@web.de>
2021-04-13 16:34:33 +0200

    Fixed restrict_armor power types disabling all armor for slots where conditions aren't specified

--- src/main/java/io/github/apace100/origins/power/RestrictArmorPower.java
+++ src/main/java/io/github/apace100/origins/power/RestrictArmorPower.java
@@ -32,7 +32,7 @@ public class RestrictArmorPower extends Power {
     }
 
     public boolean canEquip(ItemStack itemStack, EquipmentSlot slot) {
-        if (armorConditions.get(slot) == null) return false;
+        if (armorConditions.get(slot) == null) return true;
         return !armorConditions.get(slot).test(itemStack);
     }
 }
