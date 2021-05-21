61d9dca0d8252786a64487281488c4464b804424
Apace <apace100@web.de>
2021-05-21 18:45:29 +0200

    Fixed `modify_projectile_damage` with damage reduced to 0 still applying a hit

--- src/main/java/io/github/apace100/origins/mixin/PersistentProjectileEntityMixin.java
+++ src/main/java/io/github/apace100/origins/mixin/PersistentProjectileEntityMixin.java
@@ -7,10 +7,12 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.hit.EntityHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(PersistentProjectileEntity.class)
 public abstract class PersistentProjectileEntityMixin {
@@ -24,5 +26,13 @@ public abstract class PersistentProjectileEntityMixin {
             return (int) OriginComponent.modify(owner, ModifyProjectileDamagePower.class, original, p -> p.doesApply(source, original, target instanceof LivingEntity ? (LivingEntity)target : null), p -> p.executeActions(target));
         }
         return original;
+    }
+
+    @Redirect(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
+    private boolean preventDamageWhenZero(Entity entity, DamageSource source, float amount) {
+        if(entity instanceof ServerPlayerEntity || amount > 0f) {
+            return entity.damage(source, amount);
+        }
+        return false;
     }
 }
