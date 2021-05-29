c3b3b3058195bc29d32a6e6e4dc68da05c466a58
Aelpecyem <aelpecyem@gmail.com>
2021-05-29 13:09:20 +0200

    fix #310

--- src/main/java/io/github/apace100/origins/mixin/LivingEntityRendererMixin.java
+++ src/main/java/io/github/apace100/origins/mixin/LivingEntityRendererMixin.java
@@ -6,6 +6,7 @@ import io.github.apace100.origins.power.ModelColorPower;
 import io.github.apace100.origins.power.ShakingPower;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -14,15 +15,15 @@ import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import java.util.List;
 
@@ -59,19 +60,20 @@ public abstract class LivingEntityRendererMixin extends EntityRenderer<LivingEnt
     }
 
     @Environment(EnvType.CLIENT)
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/model/EntityModel;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;IIFFFF)V", ordinal = 0))
-    private void renderColorChangedModel(EntityModel model, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha, LivingEntity player) {
-        if(player instanceof PlayerEntity) {
+    @ModifyArgs(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/model/EntityModel;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;IIFFFF)V"))
+    private void renderColorChangedModel(Args args, LivingEntity player, float f, float tickDelta, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+        if (player instanceof PlayerEntity) {
             List<ModelColorPower> modelColorPowers = OriginComponent.getPowers(player, ModelColorPower.class);
             if (modelColorPowers.size() > 0) {
                 float r = modelColorPowers.stream().map(ModelColorPower::getRed).reduce((a, b) -> a * b).get();
                 float g = modelColorPowers.stream().map(ModelColorPower::getGreen).reduce((a, b) -> a * b).get();
                 float b = modelColorPowers.stream().map(ModelColorPower::getBlue).reduce((a, c) -> a * c).get();
                 float a = modelColorPowers.stream().map(ModelColorPower::getAlpha).min(Float::compare).get();
-                model.render(matrices, vertices, light, overlay, r * red, g * green, b * blue, a * alpha);
-                return;
+                args.set(4, (float) args.get(4) * r);
+                args.set(5, (float) args.get(5) * g);
+                args.set(6, (float) args.get(6) * b);
+                args.set(7, (float) args.get(7) * a);
             }
         }
-        model.render(matrices, vertices, light, overlay, red, green, blue, alpha);
     }
 }
