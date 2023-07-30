ab20fc00cd0f01a425982a51634064c2135b5ae6
eggohito <66972816+eggohito@users.noreply.github.com>
2023-07-31 01:59:50 +0800

    Update `NametagPrefixMixin` (oops)

--- src/main/java/io/github/apace100/origins/mixin/NametagPrefixMixin.java
+++ src/main/java/io/github/apace100/origins/mixin/NametagPrefixMixin.java
@@ -3,7 +3,7 @@ package io.github.apace100.origins.mixin;
 import io.github.apace100.origins.Origins;
 import io.github.apace100.origins.registry.ModComponents;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,15 +16,19 @@ public abstract class NametagPrefixMixin {
 
     @ModifyArg(method = "getDisplayName", at = @At(value = "INVOKE", target = "Lnet/minecraft/scoreboard/Team;decorateName(Lnet/minecraft/scoreboard/AbstractTeam;Lnet/minecraft/text/Text;)Lnet/minecraft/text/MutableText;"), index = 1)
     private Text appendOriginsNametagPrefix(Text name) {
-        if(Origins.config.enableNametagPrefix) {
-            LiteralText combinedPrefix = new LiteralText("");
-            ModComponents.ORIGIN.get(this).getOrigins().entrySet().stream()
-                .filter(entry -> !(entry.getKey().isHidden() || Origins.config.nametagPrefixBlacklist.contains(entry.getKey().getIdentifier().toString())))
-                .sorted(Map.Entry.comparingByKey())
-                .forEach(entry -> combinedPrefix.append(entry.getValue().getNametagPrefix()));
-            return combinedPrefix.equals(LiteralText.EMPTY) ? name : combinedPrefix.append(name);
+
+        if (!Origins.config.enableNametagPrefix) {
+            return name;
         }
-        return name;
+
+        MutableText combinedPrefix = Text.literal("");
+        ModComponents.ORIGIN.get(this).getOrigins().entrySet().stream()
+            .filter(entry -> !(entry.getKey().isHidden() || Origins.config.nametagPrefixBlacklist.contains(entry.getKey().getIdentifier().toString())))
+            .sorted(Map.Entry.comparingByKey())
+            .forEach(entry -> combinedPrefix.append(entry.getValue().getNametagPrefix()));
+
+        return combinedPrefix;
+
     }
 
 }
