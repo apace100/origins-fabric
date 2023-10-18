6a6bc3c73272f95626aa2d656facfcaf92acfc49
eggohito <66972816+eggohito@users.noreply.github.com>
2023-10-19 00:35:16 +0800

    Tweaked execution logic of the action in `origins:action_on_callback` power type * The action is now only executed if the entity condition specified in the power's `condition` field is fulfilled or absent

--- src/main/java/io/github/apace100/origins/power/OriginsCallbackPower.java
+++ src/main/java/io/github/apace100/origins/power/OriginsCallbackPower.java
@@ -1,7 +1,12 @@
 package io.github.apace100.origins.power;
 
+import io.github.apace100.apoli.data.ApoliDataTypes;
 import io.github.apace100.apoli.power.ActionOnCallbackPower;
 import io.github.apace100.apoli.power.PowerType;
+import io.github.apace100.apoli.power.factory.PowerFactory;
+import io.github.apace100.calio.data.SerializableData;
+import io.github.apace100.calio.data.SerializableDataTypes;
+import io.github.apace100.origins.Origins;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 
@@ -19,10 +24,36 @@ public class OriginsCallbackPower extends ActionOnCallbackPower {
     }
 
     public void onChosen(boolean isOrbOfOrigins) {
-        if(entityActionChosen != null) {
-            if(!isOrbOfOrigins || executeChosenWhenOrb) {
-                entityActionChosen.accept(entity);
-            }
+
+        if (this.isActive() && entityActionChosen != null && (!isOrbOfOrigins || executeChosenWhenOrb)) {
+            entityActionChosen.accept(entity);
         }
+
     }
+
+    public static PowerFactory createFactory() {
+        return new PowerFactory<>(
+            Origins.identifier("action_on_callback"),
+            new SerializableData()
+                .add("entity_action_respawned", ApoliDataTypes.ENTITY_ACTION, null)
+                .add("entity_action_removed", ApoliDataTypes.ENTITY_ACTION, null)
+                .add("entity_action_gained", ApoliDataTypes.ENTITY_ACTION, null)
+                .add("entity_action_lost", ApoliDataTypes.ENTITY_ACTION, null)
+                .add("entity_action_added", ApoliDataTypes.ENTITY_ACTION, null)
+                .add("entity_action_chosen", ApoliDataTypes.ENTITY_ACTION, null)
+                .add("execute_chosen_when_orb", SerializableDataTypes.BOOLEAN, true),
+            data -> (powerType, livingEntity) -> new OriginsCallbackPower(
+                powerType,
+                livingEntity,
+                data.get("entity_action_respawned"),
+                data.get("entity_action_removed"),
+                data.get("entity_action_gained"),
+                data.get("entity_action_lost"),
+                data.get("entity_action_added"),
+                data.get("entity_action_chosen"),
+                data.get("execute_chosen_when_orb")
+            )
+        ).allowCondition();
+    }
+
 }
