21725ef2890807483772e55c0658c05c81dcb471
eggohito <66972816+eggohito@users.noreply.github.com>
2023-10-19 00:35:16 +0800

    Tweaked execution logic of the action in `origins:action_on_callback` power type * The action is now only executed if the entity condition specified in the power's `condition` field is fulfilled or absent

--- src/main/java/io/github/apace100/origins/power/OriginsPowerTypes.java
+++ src/main/java/io/github/apace100/origins/power/OriginsPowerTypes.java
@@ -1,18 +1,13 @@
 package io.github.apace100.origins.power;
 
-import io.github.apace100.apoli.data.ApoliDataTypes;
 import io.github.apace100.apoli.power.PowerType;
 import io.github.apace100.apoli.power.PowerTypeReference;
 import io.github.apace100.apoli.power.factory.PowerFactory;
-import io.github.apace100.apoli.power.factory.action.ActionFactory;
+import io.github.apace100.apoli.power.factory.PowerFactorySupplier;
 import io.github.apace100.apoli.registry.ApoliRegistries;
-import io.github.apace100.calio.data.SerializableData;
-import io.github.apace100.calio.data.SerializableDataTypes;
 import io.github.apace100.origins.Origins;
-import net.minecraft.entity.Entity;
 import net.minecraft.registry.Registry;
 
-@SuppressWarnings("unchecked")
 public class OriginsPowerTypes {
 
     public static final PowerType<?> LIKE_WATER = new PowerTypeReference<>(Origins.identifier("like_water"));
@@ -24,28 +19,15 @@ public class OriginsPowerTypes {
     public static final PowerType<?> CONDUIT_POWER_ON_LAND = new PowerTypeReference<>(Origins.identifier("conduit_power_on_land"));
 
     public static void register() {
-        register(new PowerFactory<>(Origins.identifier("action_on_callback"),
-            new SerializableData()
-                .add("entity_action_respawned", ApoliDataTypes.ENTITY_ACTION, null)
-                .add("entity_action_removed", ApoliDataTypes.ENTITY_ACTION, null)
-                .add("entity_action_gained", ApoliDataTypes.ENTITY_ACTION, null)
-                .add("entity_action_lost", ApoliDataTypes.ENTITY_ACTION, null)
-                .add("entity_action_added", ApoliDataTypes.ENTITY_ACTION, null)
-                .add("entity_action_chosen", ApoliDataTypes.ENTITY_ACTION, null)
-                .add("execute_chosen_when_orb", SerializableDataTypes.BOOLEAN, true),
-            data ->
-                (type, player) -> new OriginsCallbackPower(type, player,
-                    (ActionFactory<Entity>.Instance)data.get("entity_action_respawned"),
-                    (ActionFactory<Entity>.Instance)data.get("entity_action_removed"),
-                    (ActionFactory<Entity>.Instance)data.get("entity_action_gained"),
-                    (ActionFactory<Entity>.Instance)data.get("entity_action_lost"),
-                    (ActionFactory<Entity>.Instance)data.get("entity_action_added"),
-                    (ActionFactory<Entity>.Instance)data.get("entity_action_chosen"),
-                    data.getBoolean("execute_chosen_when_orb")))
-            .allowCondition());
+        register(OriginsCallbackPower::createFactory);
     }
 
     private static void register(PowerFactory<?> serializer) {
         Registry.register(ApoliRegistries.POWER_FACTORY, serializer.getSerializerId(), serializer);
     }
+
+    private static void register(PowerFactorySupplier<?> supplier) {
+        register(supplier.createFactory());
+    }
+
 }
