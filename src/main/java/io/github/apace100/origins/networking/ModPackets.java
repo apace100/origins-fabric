1f923b2e9e4821d430be0658d81359372effd73e
eggohito <66972816+eggohito@users.noreply.github.com>
2023-12-27 16:35:51 +0800

    Fixed issue with syncing the badge registry to the client

--- src/main/java/io/github/apace100/origins/networking/ModPackets.java
+++ src/main/java/io/github/apace100/origins/networking/ModPackets.java
@@ -17,5 +17,4 @@ public class ModPackets {
     public static final Identifier CHOOSE_RANDOM_ORIGIN = new Identifier(Origins.MODID, "choose_random_origin");
     public static final Identifier CONFIRM_ORIGIN = ConfirmOriginS2CPacket.TYPE.getId();
     public static final Identifier PLAYER_LANDED = Origins.identifier("player_landed");
-    public static final Identifier BADGE_LIST = SyncBadgeRegistryS2CPacket.TYPE.getId();
 }
