15bcda7bbe4c24a1cb37c43d399b74ba6b8ce410
eggohito <66972816+eggohito@users.noreply.github.com>
2024-01-14 00:24:18 +0800

    Initial update to 1.20.4

--- src/main/java/io/github/apace100/origins/util/ChoseOriginCriterion.java
+++ src/main/java/io/github/apace100/origins/util/ChoseOriginCriterion.java
@@ -1,59 +1,46 @@
 package io.github.apace100.origins.util;
 
-import com.google.gson.JsonObject;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import io.github.apace100.origins.Origins;
 import io.github.apace100.origins.origin.Origin;
 import net.minecraft.advancement.criterion.AbstractCriterion;
-import net.minecraft.advancement.criterion.AbstractCriterionConditions;
-import net.minecraft.predicate.entity.AdvancementEntityPredicateDeserializer;
+import net.minecraft.predicate.entity.EntityPredicate;
 import net.minecraft.predicate.entity.LootContextPredicate;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.JsonHelper;
+import net.minecraft.util.dynamic.Codecs;
 
 import java.util.Optional;
 
 public class ChoseOriginCriterion extends AbstractCriterion<ChoseOriginCriterion.Conditions> {
 
     public static ChoseOriginCriterion INSTANCE = new ChoseOriginCriterion();
-    public static final Identifier ID = new Identifier(Origins.MODID, "chose_origin");
+    public static final Identifier ID = Origins.identifier("chose_origin");
 
     @Override
-    protected Conditions conditionsFromJson(JsonObject obj, Optional<LootContextPredicate> predicate, AdvancementEntityPredicateDeserializer predicateDeserializer) {
-        Identifier id = new Identifier(JsonHelper.getString(obj, "origin"));
-        return new Conditions(predicate, id);
+    public Codec<Conditions> getConditionsCodec() {
+        return Conditions.CODEC;
     }
 
     public void trigger(ServerPlayerEntity player, Origin origin) {
-        this.trigger(player, (conditions -> conditions.matches(origin)));
+        this.trigger(player, conditions -> conditions.matches(origin));
     }
 
-    public Identifier getId() {
-        return ID;
-    }
+    public record Conditions(Optional<LootContextPredicate> player, Identifier originId) implements AbstractCriterion.Conditions {
 
-    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
-    public static class Conditions extends AbstractCriterionConditions {
+        public static final Codec<Conditions> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            Codecs.createStrictOptionalFieldCodec(EntityPredicate.LOOT_CONTEXT_PREDICATE_CODEC, "player").forGetter(Conditions::player),
+            Identifier.CODEC.fieldOf("origin").forGetter(Conditions::originId)
+        ).apply(instance, Conditions::new));
 
-        private final Identifier originId;
-
-        public Conditions(Optional<LootContextPredicate> player, Identifier originId) {
-            super(player);
-            this.originId = originId;
+        @Override
+        public Optional<LootContextPredicate> player() {
+            return player;
         }
 
         public boolean matches(Origin origin) {
             return origin.getIdentifier().equals(originId);
-        }
-
-        @Override
-        public JsonObject toJson() {
-
-            JsonObject jsonObject = super.toJson();
-            jsonObject.addProperty("origin", originId.toString());
-
-            return jsonObject;
-
         }
 
     }
