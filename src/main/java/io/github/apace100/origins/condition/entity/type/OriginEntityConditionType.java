e052d71748d3de37aa95edd393f40dece9a58a99
eggohito <66972816+eggohito@users.noreply.github.com>
2025-01-21 19:10:48 +0800

    Bumped Apoli and mod version

--- src/main/java/io/github/apace100/origins/condition/entity/type/OriginEntityConditionType.java
+++ src/main/java/io/github/apace100/origins/condition/entity/type/OriginEntityConditionType.java
@@ -1,6 +1,7 @@
 package io.github.apace100.origins.condition.entity.type;
 
 import io.github.apace100.apoli.condition.ConditionConfiguration;
+import io.github.apace100.apoli.condition.context.EntityConditionContext;
 import io.github.apace100.apoli.condition.type.EntityConditionType;
 import io.github.apace100.apoli.data.TypedDataObjectFactory;
 import io.github.apace100.calio.data.SerializableData;
@@ -40,9 +41,11 @@ public class OriginEntityConditionType extends EntityConditionType {
     }
 
     @Override
-    public boolean test(Entity entity) {
+    public boolean test(EntityConditionContext context) {
 
+        Entity entity = context.entity();
         OriginComponent originComponent = ModComponents.ORIGIN.getNullable(entity);
+
         if (originComponent == null) {
             return false;
         }
