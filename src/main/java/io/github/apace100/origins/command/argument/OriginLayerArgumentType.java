65c6bc95307ccfe7984a75d09ee7faf7d458d7b4
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2025-04-09 21:53:29 +1000

    Fixed mcfunction loading issues with the origin & layer argument types
    Fixes apace100/origins-fabric#822
    See apace100/apoli@27e8ce8fe58bcd06b54b98dd202515322005b6b4

--- src/main/java/io/github/apace100/origins/command/argument/OriginLayerArgumentType.java
+++ src/main/java/io/github/apace100/origins/command/argument/OriginLayerArgumentType.java
@@ -10,13 +10,12 @@ import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import io.github.apace100.origins.origin.OriginLayer;
 import io.github.apace100.origins.origin.OriginLayerManager;
 import net.minecraft.command.CommandSource;
-import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import java.util.concurrent.CompletableFuture;
 
-public class OriginLayerArgumentType implements ArgumentType<OriginLayer> {
+public class OriginLayerArgumentType implements ArgumentType<Identifier> {
 
    public static final DynamicCommandExceptionType LAYER_NOT_FOUND = new DynamicCommandExceptionType(
        o -> Text.translatable("commands.origin.layer_not_found", o)
@@ -26,16 +25,16 @@ public class OriginLayerArgumentType implements ArgumentType<OriginLayer> {
       return new OriginLayerArgumentType();
    }
 
-   public static OriginLayer getLayer(CommandContext<ServerCommandSource> context, String argumentName) {
-      return context.getArgument(argumentName, OriginLayer.class);
-   }
-
-   @Override
-   public OriginLayer parse(StringReader reader) throws CommandSyntaxException {
-      Identifier id = Identifier.fromCommandInputNonEmpty(reader);
+   public static <S> OriginLayer getLayer(CommandContext<S> context, String argumentName) throws CommandSyntaxException {
+      Identifier id = context.getArgument(argumentName, Identifier.class);
       return OriginLayerManager.getResult(id)
           .result()
           .orElseThrow(() -> LAYER_NOT_FOUND.create(id));
+   }
+
+   @Override
+   public Identifier parse(StringReader reader) throws CommandSyntaxException {
+      return Identifier.fromCommandInputNonEmpty(reader);
    }
 
    @Override
