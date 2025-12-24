3e64c802ebc4baf39ddbab26e65e0862b3aa3b50
eggohito <66972816+eggohito@users.noreply.github.com>
2025-12-24 15:34:46 +0800

    Fixed issue with players choosing an origin in an origin layer Now it properly accounts if the player actually has all the origins before determining that the player should no longer be selecting an origin

--- src/main/java/io/github/apace100/origins/networking/ModPacketsC2S.java
+++ src/main/java/io/github/apace100/origins/networking/ModPacketsC2S.java
@@ -94,7 +94,7 @@ public class ModPacketsC2S {
 
             confirmOrigin(player, layer, originComponent.getOrigin(layer));
 
-            originComponent.selectingOrigin(false);
+            originComponent.selectingOrigin(!originComponent.hasAllOrigins());
             originComponent.sync();
 
         }
